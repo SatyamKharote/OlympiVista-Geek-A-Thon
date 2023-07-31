c4f8383a0c820e74d5e1679b5fa7e2c3f409b856
Satyam Kharote <satyamkharote123@gmail.com>
2023-08-01 03:29:46 +0530

    adding screenshot of project

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const frontendPath = path.join(__dirname, "/dist");
 app.use(express.static(frontendPath));
 // Connect to MongoDB
@@ -32,13 +29,12 @@ mongoose
     process.exit(1);
   });
 
-  const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 // Use the athleteRoutes
 app.use("/api", athleteRoutes);
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
+});
 
 app.listen(PORT, () => {
   console.log(`Server Running in ${process.env.NODE_ENV} mode On Port ${PORT}`.white.bold);
