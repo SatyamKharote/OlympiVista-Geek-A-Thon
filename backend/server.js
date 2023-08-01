54becb8a1bf54587b55b724d28f9afa1beea41bb
Satyam Kharote <satyamkharote123@gmail.com>
2023-08-01 22:02:28 +0530

    change 101 server

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const frontendPath = path.join(__dirname, "/dist");
 app.use(express.static(frontendPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 
