c97d57bbb4763483250d50634f3156d99c750805
Satyam Kharote <satyamkharote123@gmail.com>
2023-08-02 23:48:03 +0530

    server.js change

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,9 @@ app.use("/api", athleteRoutes);
 const frontendPath = path.join(__dirname, "dist");
 app.use(express.static(frontendPath));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
 
 
 
